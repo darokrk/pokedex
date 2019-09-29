2937f5b97884428b279c1fcff905a0b613b298c2
Dariusz Rosiek <daro.krk@interia.pl>
2019-09-29 19:49:18 +0200

    Build files for GitHub pages

---
+++ precache-manifest.39bf7dd6e5b42158baf7979f35b8a8e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4fa2636c55c579fbaa54eb6c7c4c84",
+    "url": "/pokemon-game/index.html"
+  },
+  {
+    "revision": "16258ab08cd319751c33",
+    "url": "/pokemon-game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "16258ab08cd319751c33",
+    "url": "/pokemon-game/static/js/2.9dade11b.chunk.js"
+  },
+  {
+    "revision": "91f905839624606b6a97",
+    "url": "/pokemon-game/static/js/main.55ada412.chunk.js"
+  },
+  {
+    "revision": "e447c387fb7d296327a7",
+    "url": "/pokemon-game/static/js/runtime~main.d620281c.js"
+  },
+  {
+    "revision": "30de278c8a2a82993631375e0421e705",
+    "url": "/pokemon-game/static/media/pattern-bg.30de278c.jpg"
+  }
+]);
