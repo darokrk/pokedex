3c11a2fa1d5249b4d30840fc2ea77f4e528f7087
Dariusz Rosiek <daro.krk@interia.pl>
2019-11-04 20:50:36 +0100

    Added hooks to Pokemon.js

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+
 import { getPokemonData, getPokemonSpecies } from "../../actions/index";
 
 import styled from "styled-components";
@@ -27,72 +28,70 @@ const mapStateToProps = ({ pokemon, pokemonSpec }) => {
   };
 };
 
-export class Pokemon extends Component {
-  componentDidMount() {
-    const { pokemonIndex } = this.props.match.params;
-    this.props.getPokemonData(pokemonIndex);
-    this.props.getPokemonSpecies(pokemonIndex);
-  }
+const Pokemon = ({
+  types,
+  history,
+  imageUrl,
+  name,
+  pokemon,
+  pokemonDesc,
+  pokemonSpec,
+  getPokemonData,
+  getPokemonSpecies,
+  match
+}) => {
+  useEffect(() => {
+    getPokemonData(match.params.pokemonIndex);
+    getPokemonSpecies(match.params.pokemonIndex);
+  }, [getPokemonData, getPokemonSpecies, match.params.pokemonIndex]);
 
-  render() {
-    const {
-      pokemonIndex,
-      types,
-      history,
-      imageUrl,
-      name,
-      pokemon,
-      pokemonDesc,
-      pokemonSpec
-    } = this.props;
-    return (
-      <div className="col-12 col-md-10 mx-auto my-3">
-        <div className="card">
-          <div className="card-header">
-            <div className="row">
-              <div className="col-12 col-md-4">
-                <h5 className="m-0">{`Pokemon number ${pokemonIndex}`}</h5>
-              </div>
-              <div className="col-8 col-md-5 d-flex justify-content-center align-items-center">
-                <PokemonType types={types} />
-              </div>
-              <div className="col-3 d-flex justify-content-end">
-                <StyledDataButton
-                  type="button"
-                  className="btn btn-danger btn-sm"
-                  onClick={history.goBack}
-                >
-                  x
-                </StyledDataButton>
-              </div>
+  return (
+    <div className="col-12 col-md-10 mx-auto my-3">
+      <div className="card">
+        <div className="card-header">
+          <div className="row">
+            <div className="col-12 col-md-4">
+              <h5 className="m-0">{`Pokemon number ${match.params.pokemonIndex}`}</h5>
             </div>
-          </div>
-          <div className="card-body">
-            <div className="row align-items-center">
-              <div className="col-md-3">
-                <PokemonImg imageUrl={imageUrl} imageAlt={name} />
-              </div>
-              <div className="col-md-9">
-                <PokemonName name={name} />
-                <PokemonStats pokemon={pokemon} />
-              </div>
-              <div className="row mt-1">
-                <PokemonDesc desc={pokemonDesc} />
-              </div>
+            <div className="col-8 col-md-5 d-flex justify-content-center align-items-center">
+              <PokemonType types={types} />
             </div>
-          </div>
-          <hr />
-          <div className="card-body">
-            <h5 className="card-title text-center">Profile</h5>
-            <div className="row">
-              <PokemonSpecies pokemonSpec={pokemonSpec} />
+            <div className="col-3 d-flex justify-content-end">
+              <StyledDataButton
+                type="button"
+                className="btn btn-danger btn-sm"
+                onClick={history.goBack}
+              >
+                x
+              </StyledDataButton>
             </div>
           </div>
         </div>
+        <div className="card-body">
+          <div className="row align-items-center">
+            <div className="col-md-3">
+              <PokemonImg imageUrl={imageUrl} imageAlt={name} />
+            </div>
+            <div className="col-md-9">
+              <PokemonName name={name} />
+              <PokemonStats pokemon={pokemon} />
+            </div>
+            <div className="row mt-1">
+              <PokemonDesc desc={pokemonDesc} />
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="card-body">
+          <h5 className="card-title text-center">Profile</h5>
+          <div className="row">
+            <PokemonSpecies pokemonSpec={pokemonSpec} />
+          </div>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default connect(
   mapStateToProps,
