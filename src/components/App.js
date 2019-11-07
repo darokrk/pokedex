bf8a50275aff57f4f3c6f5f4b238918eddb0c857
Dariusz Rosiek <daro.krk@interia.pl>
2019-11-07 20:26:49 +0100

    Added AppLogo component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import AppLogo from "./AppLogo/AppLogo";
 import Search from "./SearchPokemon/SearchPokemon";
 import PokemonsList from "./PokemonsList/PokemonsList";
 import Pokemon from "../components/Pokemon/Pokemon";
@@ -13,7 +14,7 @@ const App = () => (
   <BrowserRouter>
     <div className="row mt-5">
       <div className="col-md-4 offset-md-1">
-        <h2>Pokedex App</h2>
+        <AppLogo />
         <Alert />
       </div>
       <div className="col-md-4 offset-md-1">
---
+++ src/components/AppLogo/AppLogo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppLogo = () => {
+  return <h1>Pokedex App</h1>;
+};
+
+export default AppLogo;
